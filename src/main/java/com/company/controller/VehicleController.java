f1fe32df2598fa76bd387caeff068f45aaeb8e9c
DJ Garcia <dedjer@gmail.com>
2018-09-12 20:13:23 -0500

    Add more REST commands Changed database access for user to "update"

--- src/main/java/com/company/controller/VehicleController.java
+++ src/main/java/com/company/controller/VehicleController.java
@@ -1,20 +1,44 @@
 package com.company.controller;
 
+import com.company.repository.VehicleRepository;
 import com.company.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import com.company.entity.Vehicle;
 
 @RestController
+@RequestMapping(path="/vehicle")        //Add path for this controller
 public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
 
-    @GetMapping("/example")
-    public String example() {
-        vehicleService.saveVehicle();
+    @Autowired
+    private VehicleRepository vehicleRepository;
 
+    // Test saving a hardcoded vehicle to the database
+    @GetMapping(path="/example")
+    public String save() {
+        vehicleService.saveVehicle();
         return ("Saved");
+    }
+
+    // Get All Vehicles in JSON
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Vehicle> getAllVehicles() {
+        return vehicleRepository.findAll();
+    }
+
+    // Add a Vehicle
+    @PostMapping(path="/add")
+    Vehicle addVehicle(@RequestBody Vehicle v) {
+        return vehicleRepository.save(v);
+    }
+
+    // Delete a Vehicle
+    @DeleteMapping("/remove/{id}")
+    void deleteVehicle(@PathVariable int id) {
+        vehicleRepository.deleteById(id);
     }
 }
