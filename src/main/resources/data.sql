1e06230eb9057e6d2be16aa7ed2bdb9247feb596
DJ Garcia <dedjer@gmail.com>
2018-09-30 14:29:29 -0500

    Setup project to run data.sql to pre-load data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into db_example.customers (customer_id, fullname) values (1,'DJ');
+insert into db_example.customers (customer_id, fullname) values (2,'Tricia');
+
+insert into db_example.addresses (address_id, city, state, street, zipcode) values (1, 'Dallas', 'Texas', 'Galleria Dr', '75240');
+insert into db_example.addresses (address_id, city, state, street, zipcode) values (2, 'Plano', 'Texas', 'Legacy Rd', '75024');
